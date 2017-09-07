168e1368ce6c5f489eb3e4a44529066af4136158
Sudheera Daggumalli <sudheeradaggumalli@gmail.com>
2017-09-07 04:15:33 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,22 @@
+def turn_count(board)
+    counter=0
+    counter=1
+    board.each do |value|
+      if value=="X" || value=="O"
+        counter1+=1
+        return counter1
+      end
+
+      end
+    end
+
+end
+
+def current_player(board)
+    value=turn_count(board)
+    if value % 2==0
+      "X"
+    else
+      "O"
+    end
+end
